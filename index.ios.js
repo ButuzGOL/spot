d70668b26a2e7940af42b6bb8be2ae2129aa107b
ButuzGOL <ButuzGOL@gmail.com>
2016-01-18 09:32:08 +0200

    Getting location

--- index.ios.js
+++ index.ios.js
@@ -1,29 +1,92 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  AppStateIOS
 } from 'react-native';
 
+var { DeviceEventEmitter } = React;
+
+import Parse from 'parse/react-native';
+
+var { RNLocation: Location } = require('NativeModules');
+
+Parse.initialize("TYkg9kbczuIuPZt7AvIxCtFBido4wEcBYB5ckSvA", "t0XSjbX0s0FrmGrSMTFu1cPByWtSx1LeCHcbcTxF");
+
+var COUNT = 0;
+Location.requestAlwaysAuthorization();
+Location.startUpdatingLocation();
+Location.setDistanceFilter(2.0);
+
+var subscription = DeviceEventEmitter.addListener(
+    'locationUpdated',
+    (location) => {
+      COUNT++;
+      console.log('updated');
+      var TestObject = Parse.Object.extend("TestObject");
+      var testObject = new TestObject();
+      testObject.save({bar: JSON.stringify(location.coords)}).then(function(object) {
+        console.log('saved');
+        alert("yay! it worked");
+      }, () => {
+        console.log('not saved');
+        alert("yay! it not worked");
+      });
+    }
+);
+
 class spot extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialPosition: '',
+      lastPosition: ''
+    };
+  }
+  componentDidMount() {
+  }
+
+  watchPos() {
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      const lastPosition = JSON.stringify(position);
+      this.setState({ lastPosition });
+
+      var TestObject = Parse.Object.extend("TestObject");
+      var testObject = new TestObject();
+      testObject.save({bar: lastPosition}).then(function(object) {
+        alert("yay! it worked");
+      });
+    });
+  }
+
+  getPos() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const initialPosition = JSON.stringify(position);
+        this.setState({ initialPosition });
+
+        var TestObject = Parse.Object.extend("TestObject");
+        var testObject = new TestObject();
+        testObject.save({foo: initialPosition}).then(function(object) {
+          alert("yay! it worked");
+        });
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <View>
+        <Text>
+          <Text style={styles.title}>Initial position: </Text>
+          {this.state.initialPosition}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text>
+          <Text style={styles.title}>Current position: </Text>
+          {this.state.lastPosition}
         </Text>
       </View>
     );
@@ -31,21 +94,8 @@ class spot extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  title: {
+    fontWeight: '500',
   },
 });
 
